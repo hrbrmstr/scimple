33b5338e6dc3cd61bea9a98faff6a5adcaaf4b79
boB Rudis <bob@rud.is>
2017-03-04 00:25:41 -0500

    fixed CRAN checks

--- R/zzz.r
+++ R/zzz.r
@@ -2,10 +2,11 @@ ci_funcs <- c(scimp_fs, scimp_goodman, scimp_qh, scimp_sg, scimp_wald, scimp_wal
 names(ci_funcs) <- c("fs", "goodman", "qh", "sg", "wald", "waldcc", "wilson")
 
 #' @title Simple tranlsation table from method shorthand to full method name
+#' @description Useful for charts/reports
 #' @docType data
 #' @export
 #' @examples
-#' scimple_short_to_long[c("waldcc", "sg", "qh)]
+#' scimple_short_to_long[c("waldcc", "sg", "qh")]
 scimple_short_to_long <- c(wilson="Wilson", waldcc="Wald (continuity correction)",
                            wald="Wald", fs="Fitzpatrick & Scott", goodman="Goodman",
                            sg="Sison & Glaz", qh="Quesenberry and Hurst")
